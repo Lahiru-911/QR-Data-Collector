6d3ad0d3c9e4f2247f0d32c7b33d17085cb2dbbe
Lahiru Kavinda <lahiruxk@gmail.com>
2025-01-06 12:09:49 +0530

    Update Form.jsx

--- src/Pages/Form.jsx
+++ src/Pages/Form.jsx
@@ -1,13 +1,11 @@
 const Form = () => {
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center ">
-        <div className="w-full h-auto bg-white shadow-2xl ring-1 ring-gray-900/10 rounded-3xl m-2 md:m-3 xl:m-3">
-          <div className="bg-[#0081FB] w-full  h-24  m-2 rounded-3xl">
-            <h1>Welcome,</h1>
-            <h2>You Are Valuable to Us</h2>
-            <p>We’d like to ask you for some additional information</p>
-          </div>
+      <div className="min-h-screen w-auto h-auto bg-white shadow-2xl ring-1 ring-gray-900/10 rounded-3xl m-2 md:m-3 xl:m-3 flex flex-col items-center justify-center">
+        <div className="bg-[#0081FB] w-full  h-24  m-2 rounded-3xl">
+          <h1>Welcome,</h1>
+          <h2>You Are Valuable to Us</h2>
+          <p>We’d like to ask you for some additional information</p>
         </div>
       </div>
     </>
