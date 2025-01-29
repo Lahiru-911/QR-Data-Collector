c987f967e83186674f83dcf7804c964ee303792a
Lahiru Kavinda <lahiruxk@gmail.com>
2025-01-29 11:58:00 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./Routes/Router";
 import NavBar from "./Components/NavBar";
-import Footer from "./Components/Footer";
 
 function App() {
   return (
